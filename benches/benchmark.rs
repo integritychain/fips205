899179a1e15e8e695d562e84580f4fe89a64a4ef
eschorn1 <eric.schorn@nccgroup.com>
2024-10-01 14:56:50 -0500

    clippy/benches: shaky, shaky

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -25,18 +25,18 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     let (pk_shake_256s, sk_shake_256s) = slh_dsa_shake_256s::KG::try_keygen().unwrap();
     let (pk_shake_256f, sk_shake_256f) = slh_dsa_shake_256f::KG::try_keygen().unwrap();
 
-    let sig_sha2_128s = sk_sha2_128s.try_sign(&message, randomize).unwrap();
-    let sig_sha2_128f = sk_sha2_128f.try_sign(&message, randomize).unwrap();
-    let sig_sha2_192s = sk_sha2_192s.try_sign(&message, randomize).unwrap();
-    let sig_sha2_192f = sk_sha2_192f.try_sign(&message, randomize).unwrap();
-    let sig_sha2_256s = sk_sha2_256s.try_sign(&message, randomize).unwrap();
-    let sig_sha2_256f = sk_sha2_256f.try_sign(&message, randomize).unwrap();
-    let sig_shake_128s = sk_shake_128s.try_sign(&message, randomize).unwrap();
-    let sig_shake_128f = sk_shake_128f.try_sign(&message, randomize).unwrap();
-    let sig_shake_192s = sk_shake_192s.try_sign(&message, randomize).unwrap();
-    let sig_shake_192f = sk_shake_192f.try_sign(&message, randomize).unwrap();
-    let sig_shake_256s = sk_shake_256s.try_sign(&message, randomize).unwrap();
-    let sig_shake_256f = sk_shake_256f.try_sign(&message, randomize).unwrap();
+    let sig_sha2_128s = sk_sha2_128s.try_sign(&message, b"context", randomize).unwrap();
+    let sig_sha2_128f = sk_sha2_128f.try_sign(&message, b"context", randomize).unwrap();
+    let sig_sha2_192s = sk_sha2_192s.try_sign(&message, b"context", randomize).unwrap();
+    let sig_sha2_192f = sk_sha2_192f.try_sign(&message, b"context", randomize).unwrap();
+    let sig_sha2_256s = sk_sha2_256s.try_sign(&message, b"context", randomize).unwrap();
+    let sig_sha2_256f = sk_sha2_256f.try_sign(&message, b"context", randomize).unwrap();
+    let sig_shake_128s = sk_shake_128s.try_sign(&message, b"context", randomize).unwrap();
+    let sig_shake_128f = sk_shake_128f.try_sign(&message, b"context", randomize).unwrap();
+    let sig_shake_192s = sk_shake_192s.try_sign(&message, b"context", randomize).unwrap();
+    let sig_shake_192f = sk_shake_192f.try_sign(&message, b"context", randomize).unwrap();
+    let sig_shake_256s = sk_shake_256s.try_sign(&message, b"context", randomize).unwrap();
+    let sig_shake_256f = sk_shake_256f.try_sign(&message, b"context", randomize).unwrap();
 
     c.bench_function("sha2_128f  keygen", |b| b.iter(|| slh_dsa_sha2_128f::KG::try_keygen()));
     c.bench_function("sha2_192f  keygen", |b| b.iter(|| slh_dsa_sha2_192f::KG::try_keygen()));
@@ -52,77 +52,77 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("shake_256s keygen", |b| b.iter(|| slh_dsa_shake_256s::KG::try_keygen()));
     //
     c.bench_function("sha2_128f  sign  ", |b| {
-        b.iter(|| sk_sha2_128f.try_sign(&message, randomize))
+        b.iter(|| sk_sha2_128f.try_sign(&message, b"context", randomize))
     });
     c.bench_function("sha2_192f  sign  ", |b| {
-        b.iter(|| sk_sha2_192f.try_sign(&message, randomize))
+        b.iter(|| sk_sha2_192f.try_sign(&message, b"context", randomize))
     });
     c.bench_function("sha2_256f  sign  ", |b| {
-        b.iter(|| sk_sha2_256f.try_sign(&message, randomize))
+        b.iter(|| sk_sha2_256f.try_sign(&message, b"context", randomize))
     });
     c.bench_function("shake_128f sign  ", |b| {
-        b.iter(|| sk_shake_128f.try_sign(&message, randomize))
+        b.iter(|| sk_shake_128f.try_sign(&message, b"context", randomize))
     });
     c.bench_function("shake_192f sign  ", |b| {
-        b.iter(|| sk_shake_192f.try_sign(&message, randomize))
+        b.iter(|| sk_shake_192f.try_sign(&message, b"context", randomize))
     });
     c.bench_function("shake_256f sign  ", |b| {
-        b.iter(|| sk_shake_256f.try_sign(&message, randomize))
+        b.iter(|| sk_shake_256f.try_sign(&message, b"context", randomize))
     });
     c.bench_function("sha2_128s  sign  ", |b| {
-        b.iter(|| sk_sha2_128s.try_sign(&message, randomize))
+        b.iter(|| sk_sha2_128s.try_sign(&message, b"context", randomize))
     });
     c.bench_function("sha2_192s  sign  ", |b| {
-        b.iter(|| sk_sha2_192s.try_sign(&message, randomize))
+        b.iter(|| sk_sha2_192s.try_sign(&message, b"context", randomize))
     });
     c.bench_function("sha2_256s  sign  ", |b| {
-        b.iter(|| sk_sha2_256s.try_sign(&message, randomize))
+        b.iter(|| sk_sha2_256s.try_sign(&message, b"context", randomize))
     });
     c.bench_function("shake_128s sign  ", |b| {
-        b.iter(|| sk_shake_128s.try_sign(&message, randomize))
+        b.iter(|| sk_shake_128s.try_sign(&message, b"context", randomize))
     });
     c.bench_function("shake_192s sign  ", |b| {
-        b.iter(|| sk_shake_192s.try_sign(&message, randomize))
+        b.iter(|| sk_shake_192s.try_sign(&message, b"context", randomize))
     });
     c.bench_function("shake_256s sign  ", |b| {
-        b.iter(|| sk_shake_256s.try_sign(&message, randomize))
+        b.iter(|| sk_shake_256s.try_sign(&message, b"context", randomize))
     });
     //
     c.bench_function("sha2_128f  verify", |b| {
-        b.iter(|| pk_sha2_128f.try_verify(&message, &sig_sha2_128f))
+        b.iter(|| pk_sha2_128f.try_verify(&message, &sig_sha2_128f, b"context"))
     });
     c.bench_function("sha2_192f  verify", |b| {
-        b.iter(|| pk_sha2_192f.try_verify(&message, &sig_sha2_192f))
+        b.iter(|| pk_sha2_192f.try_verify(&message, &sig_sha2_192f, b"context"))
     });
     c.bench_function("sha2_256f  verify", |b| {
-        b.iter(|| pk_sha2_256f.try_verify(&message, &sig_sha2_256f))
+        b.iter(|| pk_sha2_256f.try_verify(&message, &sig_sha2_256f, b"context"))
     });
     c.bench_function("shake_128f verify", |b| {
-        b.iter(|| pk_shake_128f.try_verify(&message, &sig_shake_128f))
+        b.iter(|| pk_shake_128f.try_verify(&message, &sig_shake_128f, b"context"))
     });
     c.bench_function("shake_192f verify", |b| {
-        b.iter(|| pk_shake_192f.try_verify(&message, &sig_shake_192f))
+        b.iter(|| pk_shake_192f.try_verify(&message, &sig_shake_192f, b"context"))
     });
     c.bench_function("shake_256f verify", |b| {
-        b.iter(|| pk_shake_256f.try_verify(&message, &sig_shake_256f))
+        b.iter(|| pk_shake_256f.try_verify(&message, &sig_shake_256f, b"context"))
     });
     c.bench_function("sha2_128s  verify", |b| {
-        b.iter(|| pk_sha2_128s.try_verify(&message, &sig_sha2_128s))
+        b.iter(|| pk_sha2_128s.try_verify(&message, &sig_sha2_128s, b"context"))
     });
     c.bench_function("sha2_192s  verify", |b| {
-        b.iter(|| pk_sha2_192s.try_verify(&message, &sig_sha2_192s))
+        b.iter(|| pk_sha2_192s.try_verify(&message, &sig_sha2_192s, b"context"))
     });
     c.bench_function("sha2_256s  verify", |b| {
-        b.iter(|| pk_sha2_256s.try_verify(&message, &sig_sha2_256s))
+        b.iter(|| pk_sha2_256s.try_verify(&message, &sig_sha2_256s, b"context"))
     });
     c.bench_function("shake_128s verify", |b| {
-        b.iter(|| pk_shake_128s.try_verify(&message, &sig_shake_128s))
+        b.iter(|| pk_shake_128s.try_verify(&message, &sig_shake_128s, b"context"))
     });
     c.bench_function("shake_192s verify", |b| {
-        b.iter(|| pk_shake_192s.try_verify(&message, &sig_shake_192s))
+        b.iter(|| pk_shake_192s.try_verify(&message, &sig_shake_192s, b"context"))
     });
     c.bench_function("shake_256s verify", |b| {
-        b.iter(|| pk_shake_256s.try_verify(&message, &sig_shake_256s))
+        b.iter(|| pk_shake_256s.try_verify(&message, &sig_shake_256s, b"context"))
     });
 }
 
